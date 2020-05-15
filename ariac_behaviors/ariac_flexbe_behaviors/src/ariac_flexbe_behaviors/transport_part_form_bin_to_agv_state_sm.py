d624270547938a8a9e04223efaeb0f34f378f6c0
Bas <bgommers@avans.nl>
2020-05-15 13:48:34 +0200

    aanpassingen aan transport to bin

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/transport_part_form_bin_to_agv_state_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/transport_part_form_bin_to_agv_state_sm.py
@@ -8,6 +8,7 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
 from ariac_flexbe_states.message_state import MessageState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
@@ -43,11 +44,22 @@ class transport_part_form_bin_to_agv_stateSM(Behavior):
 
 
 	def create(self):
-		# x:502 y:44, x:223 y:140
+		# x:1041 y:103, x:103 y:253
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['part_type', 'agv_id', 'pose_on_agv'])
 		_state_machine.userdata.part_type = ''
 		_state_machine.userdata.agv_id = ''
 		_state_machine.userdata.pose_on_agv = []
+		_state_machine.userdata.move_group_prefix1 = '/ariac/arm1'
+		_state_machine.userdata.move_group_prefix2 = '/ariac/arm2'
+		_state_machine.userdata.move_group = 'manipulator'
+		_state_machine.userdata.tool_link = ee_link
+		_state_machine.userdata.part_pose = []
+		_state_machine.userdata.joint_values = []
+		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = ''
+		_state_machine.userdata.agv_pose = []
+		_state_machine.userdata.config_name_bin6PreGrasp = 'bin6PreGrasp'
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -56,26 +68,33 @@ class transport_part_form_bin_to_agv_stateSM(Behavior):
 
 
 		with _state_machine:
-			# x:30 y:40
-			OperatableStateMachine.add('AgvIdMessage',
-										MessageState(),
-										transitions={'continue': 'PartTypeMessage'},
-										autonomy={'continue': Autonomy.Off},
-										remapping={'message': 'agv_id'})
+			# x:314 y:92
+			OperatableStateMachine.add('MoveR1pregrasp6',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'AgvIdMessage', 'planning_failed': 'failed', 'control_failed': 'AgvIdMessage', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_bin6PreGrasp', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix1', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
-			# x:305 y:37
+			# x:872 y:87
 			OperatableStateMachine.add('MoseMessage',
 										MessageState(),
 										transitions={'continue': 'finished'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'message': 'pose_on_agv'})
 
-			# x:171 y:37
+			# x:696 y:90
 			OperatableStateMachine.add('PartTypeMessage',
 										MessageState(),
 										transitions={'continue': 'MoseMessage'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'message': 'part_type'})
+
+			# x:514 y:89
+			OperatableStateMachine.add('AgvIdMessage',
+										MessageState(),
+										transitions={'continue': 'PartTypeMessage'},
+										autonomy={'continue': Autonomy.Off},
+										remapping={'message': 'agv_id'})
 
 
 		return _state_machine
